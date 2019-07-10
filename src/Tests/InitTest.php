a289a95039bce337f5ad1391ce0485986994be06
Màrius <marius.asensi@gmail.com>
2019-07-10 10:04:18 +0200

    feature: add phpunit config test: add example test

---
+++ src/Tests/InitTest.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class InitTest extends TestCase
+{
+    public function testCorrect()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
